390ef0d621b673ad738e7063f439521c2f264d5d
Emily Chiu <emily.cindy.chiu@gmail.com>
2021-11-04 12:52:02 -0700

    added start endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import * as express from 'express'
 import * as cors from 'cors'
 import { v4 as uuidv4 } from 'uuid';
 
+// import state, updateState, Action from new file called update-state?
+
 const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -17,11 +19,6 @@ export const logger: Logger = function (...args) {
 let cache = ['hello']
 const myDB = {}
 
-
-setInterval(() => {
-    console.log('hello')
-}, 1000)
-
 const origin = `http://localhost:1234`
 const corsOptions = {
     origin,
@@ -29,6 +26,42 @@ const corsOptions = {
 
 }
 app.use(cors(corsOptions))
+
+//START ENDPOINTS HERE
+app.post('/start', (req, res) => {
+    console.log('boo')
+    const gameId = req.body.userId || uuidv4()
+    const userId = req.body.userId || uuidv4()
+
+    // from the frontend: 
+    //const responsePromise = axios.post<{state: BrowserState}>('http://localhost:3000/make-a-move', { stateofBoard: state.stateOfBoard, userId: state.userId, gameId: state.gameId, gamesWaitingForPlayer})
+
+    // update the state with: 
+    if (gamesWaitingForPlayer) {
+        state = updateState({action: Action.start, payload: gameId})
+    }
+
+    state = updateState({action: Action.join, payload: userId})
+
+    let gamePiece
+ 
+    if (state.players.X === userId) {
+        gamePiece = 'X'
+        // is there something else to add re: which color the board is? boardColor = green here?
+    } else {
+        state.players.O = userId
+        gamePiece = 'O'
+        // boardColor = red?
+    }
+    
+    // wipe moves
+    state.stateofBoard = []
+
+    const browserState = convertStatetoBrowserState(userId, gamePiece, state)
+    res.send(browserState)
+})
+
+
 
 app.listen(port, () => {
     logger(`Example app listening on port ${port}!`)
